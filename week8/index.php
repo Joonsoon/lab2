271e5e53c48d3b5a894d157e9857a0832178dd13
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-01-25 22:37:29 +0800

    added an assigned string to a variable variable

--- week8/index.php
+++ week8/index.php
@@ -8,8 +8,13 @@
 <body style="background-color: black; color: white; text-align: center; padding-top: 50px; font-family: Arial, sans-serif;">
 
     <?php
+    // Assigning a string to a variable
+    $enrollmentStatus = "Currently Enrolled In Asia Pacific College";
+
+    // Displaying the variable in HTML
     echo "<h1>Janson Pagharion</h1>";
     echo "<p>BSCS-SF221</p>";
+    echo "<p>$enrollmentStatus</p>";
     ?>
 
 </body>
