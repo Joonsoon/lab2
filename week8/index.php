68db8b3c38b7fb8fef7d5687792f480a35f0e182
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-01-25 22:34:12 +0800

    added php file with 2 php string inserts

---
+++ week8/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Page</title>
+</head>
+
+<body style="background-color: black; color: white; text-align: center; padding-top: 50px; font-family: Arial, sans-serif;">
+
+    <?php
+    echo "<h1>Janson Pagharion</h1>";
+    echo "<p>BSCS-SF221</p>";
+    ?>
+
+</body>
+
+</html>
