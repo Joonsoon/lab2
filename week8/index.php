81e95e9b8bf2addef1577d7620b97b4938b366af
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-01-25 22:41:53 +0800

    Added a for loop that generates a random amount of paragraphs, Also added a PHP border around the texts

--- week8/index.php
+++ week8/index.php
@@ -3,19 +3,37 @@
 
 <head>
     <title>PHP Page</title>
+    <style>
+        .container {
+            width: 60%;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid white;
+            border-radius: 10px;
+        }
+    </style>
 </head>
 
 <body style="background-color: black; color: white; text-align: center; padding-top: 50px; font-family: Arial, sans-serif;">
 
-    <?php
-    // Assigning a string to a variable
-    $enrollmentStatus = "Currently Enrolled In Asia Pacific College";
+    <div class="container">
+        <?php
+        // Assigning a string to a variable
+        $enrollmentStatus = "Currently Enrolled In Asia Pacific College";
 
-    // Displaying the variable in HTML
-    echo "<h1>Janson Pagharion</h1>";
-    echo "<p>BSCS-SF221</p>";
-    echo "<p>$enrollmentStatus</p>";
-    ?>
+        // Displaying the variable in HTML
+        echo "<h1>Janson Pagharion</h1>";
+        echo "<p>BSCS-SF221</p>";
+        echo "<p>$enrollmentStatus</p>";
+
+        // Adding a random PHP loop to generate paragraphs
+        $randomParagraphs = rand(1, 5);
+
+        for ($i = 0; $i < $randomParagraphs; $i++) {
+            echo "<p>This is a random paragraph #$i.</p>";
+        }
+        ?>
+    </div>
 
 </body>
 
