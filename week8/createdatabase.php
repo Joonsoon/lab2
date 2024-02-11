9fe68449b2e1fb1baca4b9073e308ebf6a4d03a4
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-02-12 01:03:11 +0800

    fixed createdatabase.php

--- week8/createdatabase.php
+++ week8/createdatabase.php
@@ -2,28 +2,20 @@
 $servername = "localhost";
     $username = "webprogmi222_sf221";
     $password = "xE*Y2nleNVvZm[!!";
-    $dbname = "webprogmi222_sf221";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE jhpagharion2_myguests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
+// Create database
+$sql = "CREATE DATABASE myDB";
 if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
+  echo "Database created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error creating database: " . $conn->error;
 }
 
 $conn->close();
